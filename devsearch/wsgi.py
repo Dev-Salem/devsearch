2f518fbc1b80b30e3c631c0e14fb110b56d9ca72
Dev Salem <dev.salemsaleh@gmail.com>
2024-06-14 19:47:29 +0300

    Deploy with Render

--- devsearch/wsgi.py
+++ devsearch/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+print("WSGI module loaded", file=sys.stderr)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devsearch.settings')
 
 application = get_wsgi_application()
+
+print("Application loaded", file=sys.stderr)
